955fc59d5837d29373847d1845f1c935acf45f70
milliorn <scottmilliorn@gmai.com>
2023-08-27 22:51:18 -0700

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Define a state to track whether the sidebar is open or closed
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // Function to toggle the sidebar
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="md:flex">
+        {/* Mobile Navbar (Visible on Mobile) */}
+        <nav className={`md:hidden bg-gray-800`}>
+          {/* Hamburger Button */}
+          <div className="flex justify-between items-center py-2 px-4">
+            <div className="text-white text-2xl font-bold">
+              True Hope 4 Success
+            </div>
+            <button
+              onClick={toggleSidebar}
+              className="text-gray-500 hover:text-white focus:outline-none focus:text-white"
+            >
+              <svg
+                className="h-6 w-6 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                {/* Hamburger Icon */}
+                {isSidebarOpen ? (
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M18 14H6v-1h12zm0-5H6V8h12zm0-4H6V4h12z"
+                  />
+                ) : (
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4 6h16v1H4zm0 5h16v-1H4zm0 5h16v-1H4z"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </nav>
+
+        {/* Sidebar (Hidden on Mobile) */}
+        <aside
+          className={`md:block w-1/5 bg-gray-800 h-screen ${
+            isSidebarOpen ? "block" : "hidden"
+          }`}
+        >
+          {/* Sidebar content */}
+          <ul className="text-white ml-2">
+            {/* List of clickable anchor elements */}
+            <li className="mb-2">
+              <a href="#about" className="block hover:text-blue-400">
+                About
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#skills" className="block hover:text-blue-400">
+                Skills
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#education" className="block hover:text-blue-400">
+                Education
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="#contact" className="block hover:text-blue-400">
+                Contact
+              </a>
+            </li>
+            {/* Add more sections as needed */}
+          </ul>
+        </aside>
+
+        {/* Main Content */}
+        <main className="w-full md:w-4/5 bg-white p-8">
+          {/* Main content goes here */}
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
