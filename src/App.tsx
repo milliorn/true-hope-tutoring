770892bc035b00f9790612d0d89cb697b685e1e3
milliorn <scottmilliorn@gmai.com>
2023-08-28 22:32:56 -0700

    learn more button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+const NAME_FIRST = "Hope";
+const NAME_LAST = "True";
+
 function App() {
   // Define a state to track whether the sidebar (navbar) is open or closed
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -7,6 +10,11 @@ function App() {
   // Function to toggle the sidebar (navbar)
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const scrollToAbout = () => {
+    const about = document.getElementById("about");
+    about?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -17,8 +25,8 @@ function App() {
           className={`md:hidden`}
           style={{
             background: isSidebarOpen
-              ? "linear-gradient(to bottom, #111827, #6B7280)"
-              : "linear-gradient(to bottom, #111827, #111827)",
+              ? "linear-gradient(to bottom, #082f49, #6B7280)"
+              : "linear-gradient(to bottom, #082f49, #082f49)",
           }}
         >
           {/* Hamburger Button */}
@@ -84,11 +92,29 @@ function App() {
         </nav>
 
         {/* Sidebar (Hidden on Mobile) */}
-        {/* You can remove this section as it's no longer needed */}
 
-        {/* Main Content */}
-        <main className="w-full md:w-4/5 bg-sky-100 h-screen p-8">
+        {/* Centered Content */}
+        <main className="w-full md:w-4/5 bg-sky-50 h-screen flex justify-center items-center p-8">
           {/* Main content goes here */}
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">
+              {NAME_FIRST} <span className="text-sky-400">{NAME_LAST}</span>
+            </h1>
+            <h2 className="text-2xl font-bold capitalize pt-4">
+              Private <span className="text-sky-400">Math</span> Tutor{" "}
+              <span className="text-sky-400">&amp;</span>
+            </h2>
+            <h3 className="font-bold capitalize text-center pt-4 text-lg">
+              True Hope <span className="text-sky-400">4</span> Success Tutoring
+            </h3>
+            {/* Learn More Button */}
+            <button
+              onClick={scrollToAbout}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-1000 ease-in-out inline-block m-4"
+            >
+              Learn More
+            </button>
+          </div>
         </main>
       </div>
     </div>
