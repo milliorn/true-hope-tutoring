b5c9f8384abeb514e7eb277d0010aed5b220ba52
milliorn <scottmilliorn@gmai.com>
2023-09-14 15:49:38 -0700

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/true-hope-tutoring/",
+  // base: "/true-hope-tutoring/", this is for github pages
   plugins: [ react() ],
 })
